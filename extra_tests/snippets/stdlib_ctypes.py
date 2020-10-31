8d21ed482acdd9407c01df965871202b2149d1a2
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-10-31 18:23:29 -0300

    Add __getattr__ to deal with attributes at runtime

--- extra_tests/snippets/stdlib_ctypes.py
+++ extra_tests/snippets/stdlib_ctypes.py
@@ -3,5 +3,6 @@ import ctypes
 # ctypes.CDLL("/home/shammyz/Documents/sandbox/libhello.so")
 # ctypes.dlopen("/home/shammyz/Documents/sandbox/libhello.so")
 lib = ctypes.cdll("/home/shammyz/Documents/sandbox/libhello.so")
-lib.tt()
+lib.hello()
+lib.ola()
 assert False
---
+++ vm/src/stdlib/ctypes/function.rs
@@ -0,0 +1,30 @@
+use crate::VirtualMachine;
+use crate::builtins::PyTypeRef;
+use crate::pyobject::{PyValue, StaticType};
+
+#[pyclass(module = "_ctypes", name = "CFuncPtr")]
+#[derive(Debug)]
+pub struct PyCFuncPtr {
+    ext_func: extern "C" fn(),
+}
+
+impl PyValue for PyCFuncPtr {
+    fn class(vm: &VirtualMachine) -> &PyTypeRef {
+        Self::static_type()
+    }
+}
+
+#[pyimpl]
+impl PyCFuncPtr {
+    #[inline]
+    pub fn new(ext_func: extern "C" fn()) -> PyCFuncPtr {
+        PyCFuncPtr {
+            ext_func
+        }
+    }
+
+    #[pymethod]
+    pub fn call(&self) {
+        (self.ext_func)();
+    }
+}
