72b672cfab881cf9374ca8477f77280bf595df77
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-11-03 19:33:37 -0300

    Add PyFuncPtr tp_new from DLL and basic tp_call

--- extra_tests/snippets/stdlib_ctypes.py
+++ extra_tests/snippets/stdlib_ctypes.py
@@ -2,7 +2,6 @@ import ctypes
 
 # ctypes.CDLL("/home/shammyz/Documents/sandbox/libhello.so")
 # ctypes.dlopen("/home/shammyz/Documents/sandbox/libhello.so")
-lib = ctypes.cdll("/home/shammyz/Documents/sandbox/libhello.so")
+lib = ctypes.CDLL("/home/shammyz/Documents/sandbox/libhello.so")
 lib.hello()
-lib.ola()
 assert False
