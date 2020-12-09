e5f03ea1c6481f3723145aceb79e08898daeaae3
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-12-09 10:58:23 -0300

    Remove unused files

--- extra_tests/snippets/stdlib_ctypes.py
+++
@@ -1,7 +0,0 @@
-import ctypes
-
-# ctypes.CDLL("/home/shammyz/Documents/sandbox/libhello.so")
-# ctypes.dlopen("/home/shammyz/Documents/sandbox/libhello.so")
-lib = ctypes.CDLL("/home/shammyz/Documents/sandbox/libhello.so")
-lib.hello()
-assert False
